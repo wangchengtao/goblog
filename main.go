a7c5a7bc5cf0cf74691e83e21d526eab11cb0b04
wangchengtao <915129420@qq.com>
2021-01-05 10:54:00 +0800

    重构 RouteName2URL

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"goblog/pkg/route"
 	"html/template"
 	"log"
 	"net/http"
@@ -17,7 +18,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var router = mux.NewRouter()
+var router *mux.Router
 var db *sql.DB
 
 func initDB() {
@@ -109,22 +110,13 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// 4. 读取成功
 		tmpl, err := template.New("show.gohtml").Funcs(template.FuncMap{
-			"RouteName2URL": RouteName2URL,
+			"RouteName2URL": route.Name2URL,
 			"Int64ToString": Int64ToString,
 		}).ParseFiles("resources/views/articles/show.gohtml")
 
 		checkError(err)
 		tmpl.Execute(w, article)
 	}
-}
-func RouteName2URL(routeName string, pairs ...string) string {
-	url, err := router.Get(routeName).URL(pairs...)
-
-	if err != nil {
-		checkError(err)
-		return ""
-	}
-	return url.String()
 }
 
 // Int64ToString 将 int64 转换为 string
@@ -434,6 +426,9 @@ func articlesDELETEHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	initDB()
 	createTables()
+
+	route.Initialize()
+	router = route.Router
 
 	router.HandleFunc("/", defaultHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
