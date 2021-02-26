a8d85c6bee6754d79d1737da1c170ad3c2532e9f
wangchengtao <915129420@qq.com>
2021-02-26 12:48:25 +0800

    模型基类和 view 包

--- pkg/types/converter.go
+++ pkg/types/converter.go
@@ -10,6 +10,10 @@ func Int64ToString(num int64) string {
 	return strconv.FormatInt(num, 10)
 }
 
+func Uint64ToString(num uint64) string {
+	return strconv.FormatUint(num, 10)
+}
+
 func StringToInt(num string) int {
 	i, err := strconv.Atoi(num)
 
