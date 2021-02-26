ca399549d877d5f9e5b2486944877441fbeca27a
wangchengtao <915129420@qq.com>
2021-02-26 10:37:33 +0800

    重构中间件

--- main.go
+++ main.go
@@ -5,33 +5,14 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
 	"goblog/pkg/database"
 	"net/http"
-	"strings"
 )
 
 var router *mux.Router
 var db *sql.DB
-
-func forceHtmlMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 2. 继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
 
 func main() {
 	database.Initialize()
@@ -39,7 +20,6 @@ func main() {
 
 	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
-	router.Use(forceHtmlMiddleware)
 
 	// 通过路由名称获取路由
 	homeUrl, _ := router.Get("about").URL()
@@ -47,7 +27,7 @@ func main() {
 	articlesUrl, _ := router.Get("articles.show").URL("id", "123")
 	fmt.Println(articlesUrl)
 
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 
 	fmt.Printf("CCTV")
 }
